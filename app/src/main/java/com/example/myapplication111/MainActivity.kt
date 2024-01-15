82ccb28cddd4c2b68f1d36b8187a3ae5accf3491
= <kolpakovstanislav3@gmail.com>
2024-01-15 11:26:15 +0300

    avt

---
+++ app/src/main/java/com/example/myapplication111/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication111
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
